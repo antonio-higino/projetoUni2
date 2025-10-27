375b0d04e97533e2d4bdf1bbb9fc66947e3960e4
Nathan Clemente <nathanmedeirosclemente@gmail.com>
2025-10-27 15:01:31 -0300

    inserção de alguns casos de teste da unidade dois e de alguns produtos.

--- src/test/java/ecommerce/service/CompraServiceTest.java
+++ src/test/java/ecommerce/service/CompraServiceTest.java
@@ -12,27 +12,73 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import ecommerce.entity.CarrinhoDeCompras;
 import ecommerce.entity.ItemCompra;
+import ecommerce.entity.Produto;
 import ecommerce.entity.Regiao;
 import ecommerce.entity.TipoCliente;
+import ecommerce.entity.TipoProduto;
 
 public class CompraServiceTest {
-	
+
 	@ParameterizedTest
-	@CsvSource({"100, 7.5, 4.5, 6, APR",
-				"100, 4, 7, 10, APR",
-				"80, 3.9, 6.1, 8, REC"})
+	@CsvSource({
+			"SUDESTE, BRONZE",
+			"SUL, PRATA",
+			"NORDESTE, OURO",
+			"CENTRO_OESTE, BRONZE",
+			"NORTE, PRATA"
+	})
 	public void deveCalcularCustoTotalCorretamente(Regiao regiao, TipoCliente tipoCliente, BigDecimal custoEsperado) {
-		//Arrange
+		// Arrange
 		CompraService service = new CompraService(null, null, null, null);
 
 		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
 
 		List<ItemCompra> itens = new ArrayList<>();
 
+		// Criar produtos de teste
+		Produto produto1 = new Produto();
+		produto1.setNome("Produto 1");
+		produto1.setPreco(new BigDecimal("50.00"));
+		produto1.setPesoFisico(new BigDecimal("1.0"));
+		produto1.setComprimento(new BigDecimal("10"));
+		produto1.setLargura(new BigDecimal("10"));
+		produto1.setAltura(new BigDecimal("10"));
+		produto1.setFragil(false);
+		produto1.setTipo(TipoProduto.ELETRONICO);
+
+		Produto produto2 = new Produto();
+		produto2.setNome("Produto 2");
+		produto2.setPreco(new BigDecimal("30.00"));
+		produto2.setPesoFisico(new BigDecimal("0.5"));
+		produto2.setComprimento(new BigDecimal("5"));
+		produto2.setLargura(new BigDecimal("5"));
+		produto2.setAltura(new BigDecimal("5"));
+		produto2.setFragil(false);
+		produto2.setTipo(TipoProduto.LIVRO);
+
+		Produto produto3 = new Produto();
+		produto3.setNome("Produto 3");
+		produto3.setPreco(new BigDecimal("20.00"));
+		produto3.setPesoFisico(new BigDecimal("0.3"));
+		produto3.setComprimento(new BigDecimal("8"));
+		produto3.setLargura(new BigDecimal("8"));
+		produto3.setAltura(new BigDecimal("8"));
+		produto3.setFragil(true);
+		produto3.setTipo(TipoProduto.ROUPA);
+
+		// Criar itens de compra
 		ItemCompra item1 = new ItemCompra();
+		item1.setProduto(produto1);
+		item1.setQuantidade(1L);
+
 		ItemCompra item2 = new ItemCompra();
+		item2.setProduto(produto2);
+		item2.setQuantidade(1L);
+
 		ItemCompra item3 = new ItemCompra();
-		// To-Do : falta setar os atributos dos itens
+		item3.setProduto(produto3);
+		item3.setQuantidade(1L);
+
 		itens.add(item1);
 		itens.add(item2);
 		itens.add(item3);
