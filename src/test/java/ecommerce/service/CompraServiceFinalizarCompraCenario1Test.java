5077d402bde9da4693c8f0e1395c14de2d15539d
Antonio Higino B L Medeiros <56856275+antonio-higino@users.noreply.github.com>
2025-12-09 16:50:22 -0300

    Início de testes com Mocks e Fakes

---
+++ src/test/java/ecommerce/service/CompraServiceFinalizarCompraCenario1Test.java
@@ -0,0 +1,30 @@
+package ecommerce.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import ecommerce.entity.CarrinhoDeCompras;
+import ecommerce.entity.ItemCompra;
+import ecommerce.entity.Produto;
+import ecommerce.entity.Regiao;
+import ecommerce.entity.TipoCliente;
+import ecommerce.entity.TipoProduto;
+
+@ExtendWith(MockitoExtension.class)
+public class CompraServiceFinalizarCompraCenario1Test {
+	
+	@Mock CarrinhoDeComprasService carrinhoService;
+	@Mock ClienteService clienteService;
+	
+	@InjectMocks CompraService service;
+}
